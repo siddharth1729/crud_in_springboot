95c33a565567322039dcb550b891e525defef075
siddharth1729 <siddharth.sabron@gmail.com>
2023-04-06 12:24:21 +0530

    Making get api in spring boot

--- src/main/java/com/crud_spring/spring/Application.java
+++ src/main/java/com/crud_spring/spring/Application.java
@@ -2,12 +2,21 @@ package com.crud_spring.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		System.out.printf("=======> Give me a bottle of rum %s!\n", name);
+		return String.format("Give me a bottle of rum %s!", name);
+	}
 }
